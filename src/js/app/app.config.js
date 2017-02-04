0ffc6c77274b58b755528002fa4997e22288ce05
Monish <monish7792@example.com>
2017-02-04 16:12:23 +0530

    Creation of angular APP

---
+++ src/js/app/app.config.js
@@ -0,0 +1,5 @@
+// Config is used to extend the app.module
+'use strict';
+// angular.module('<name of the app>',)
+ angular.module('try').
+    config(function(){});
---
+++ src/js/app/app.module.js
@@ -0,0 +1,4 @@
+// Module is used for declaring purposes
+'use strict';
+// angular.module('<name of the app>',)
+ angular.module('try', []);
