e9cf44444030d10b11dd60dbd3aaf287daf3237e
Monish <monish7792@example.com>
2017-02-05 18:11:09 +0530

    Demo on Angular Component

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,12 +1,29 @@
 'use strict';
+// sample component syntax
+// component('nameofcomponent', {
+//   template:
+//   controller: function($scope){}
+// });
  angular.module('blogList').
-      controller('blogListController', function($scope){
-        $scope.title = 'Welcome to Controller'
-        $scope.clicks = 0
-        $scope.someClickTest = function(){
-            console.log("Clicked")
-            $scope.clicks += 1
-            $scope.title = 'Clicked ' + $scope.clicks + ' times'
-        }
-
+      component('blogList', {
+          template: "<div class=''><h1>{{ title }}</h1><button type='button' name='button' ng-click='someClickTest()'>CLICK Me</button></div>",
+          controller: function($scope){
+            $scope.title = 'Welcome to Controller'
+            $scope.clicks = 0
+            $scope.someClickTest = function(){
+                console.log("Clicked")
+                $scope.clicks += 1
+                $scope.title = 'Clicked ' + $scope.clicks + ' times'
+                }
+            }
       });
+      // controller('blogListController', function($scope){
+        // $scope.title = 'Welcome to Controller'
+        // $scope.clicks = 0
+        // $scope.someClickTest = function(){
+        //     console.log("Clicked")
+        //     $scope.clicks += 1
+        //     $scope.title = 'Clicked ' + $scope.clicks + ' times'
+      //   }
+      //
+      // });
