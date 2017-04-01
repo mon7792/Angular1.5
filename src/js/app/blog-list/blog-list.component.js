fe5ef4b091768d4c7126a624d42b28b980d73420
Monish <monish7792@example.com>
2017-04-01 19:47:00 +0530

    Demo on Ng-Repeat

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -9,6 +9,13 @@
           // template: "<div class=''><h1>{{ title }}</h1><button type='button' name='button' ng-click='someClickTest()'>CLICK Me</button></div>",
           templateUrl: '/templates/blog-list.html',
           controller: function($scope){
+            var blogItems = [
+              {title: "Some Title", id: 1, description: "This is a book"},
+              {title: "Title", id: 2, description: "This is a book"},
+              {title: "Tea", id: 3, description: "This is a book"},
+              {title: "Lite", id: 4, description: "This is a book"},
+            ]
+            $scope.items = blogItems;
             $scope.title = 'Welcome to Controller'
             $scope.clicks = 0
             $scope.someClickTest = function(){
