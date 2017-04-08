578005f5f0db8e27edfba8ba1ff8e5cfb67f7527
Monish <monish7792@example.com>
2017-04-08 20:39:59 +0530

    AngularJS  Demo

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,27 +3,53 @@
  angular.module('blogDetail').
       component('blogDetail', {
           templateUrl: '/templates/blog-detail.html',
-          controller: function($location, $routeParams, $scope){
-            var blogItems = [
-              {title: "Some Title", id: 1, description: "This is a book"},
-              {title: "Title", id: 2, description: "This is a book"},
-              {title: "Tea", id: 3, description: "This is a book"},
-              {title: "Lite", id: 4, description: "This is a book"},
-            ]
-            $scope.title = 'Blog ' + $routeParams.id
-            $scope.notFound = true
-            angular.forEach(blogItems, function(post){
-              if(post.id == $routeParams.id){
-                $scope.notFound = false
-                $scope.post = post
-              }
-            })
+          controller: function($http, $location, $routeParams, $scope){
+            $http.get("/json/posts.json").then(successCallback, errorCallback);
+            function successCallback(response, status, config, statusText){
+              $scope.notFound = true
+              var blogItems =  response.data
+              $scope.posts = blogItems
+              angular.forEach(blogItems, function(post){
+                if(post.id == $routeParams.id){
+                  $scope.notFound = false
+                  $scope.post = post
+                }
+              })
+            }
+            function errorCallback(response, status, config, statusText){
+              $scope.notFound = true
+              console.log(response);
+            }
+
 
             if($scope.notFound){
               console.log("NotFound")
               // change location
               $location.path("/404")
             }
+
+
+
+
+
+
+            //
+            // $scope.title = 'Blog ' + $routeParams.id
+            // $scope.notFound = true
+            // angular.forEach(blogItems, function(post){
+            //   if(post.id == $routeParams.id){
+            //     $scope.notFound = false
+            //     $scope.post = post
+            //   }
+            // })
+            //
+            //
+            // var blogItems = [
+            //   {title: "Some Title", id: 1, description: "This is a book"},
+            //   {title: "Title", id: 2, description: "This is a book"},
+            //   {title: "Tea", id: 3, description: "This is a book"},
+            //   {title: "Lite", id: 4, description: "This is a book"}
+            // ]
 
 
             }
