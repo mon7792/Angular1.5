d340107203f239ac106fe67e9d474d741e764c22
Monish <monish7792@example.com>
2017-04-08 17:56:14 +0530

    forEach Loop Demo

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,7 +3,7 @@
  angular.module('blogDetail').
       component('blogDetail', {
           templateUrl: '/templates/blog-detail.html',
-          controller: function($routeParams, $scope){
+          controller: function($location, $routeParams, $scope){
             var blogItems = [
               {title: "Some Title", id: 1, description: "This is a book"},
               {title: "Title", id: 2, description: "This is a book"},
@@ -11,5 +11,20 @@
               {title: "Lite", id: 4, description: "This is a book"},
             ]
             $scope.title = 'Blog ' + $routeParams.id
+            $scope.notFound = true
+            angular.forEach(blogItems, function(post){
+              if(post.id == $routeParams.id){
+                $scope.notFound = false
+                $scope.post = post
+              }
+            })
+
+            if($scope.notFound){
+              console.log("NotFound")
+              // change location
+              $location.path("/404")
+            }
+
+
             }
       });
