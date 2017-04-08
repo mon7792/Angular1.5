be2e82bc25f73a5e6eb32c31840cd4132135c06e
Monish <monish7792@example.com>
2017-04-08 11:21:10 +0530

    URL Routing Part 1: Setup

--- src/js/app/app.config.js
+++ src/js/app/app.config.js
@@ -2,4 +2,25 @@
 'use strict';
 // angular.module('<name of the app>',)
  angular.module('try').
-    config(function(){});
+    config(
+      function(
+        $locationProvider,
+        $routeProvider
+      ){
+        $locationProvider.html5Mode({
+          enabled: true
+        })
+        $routeProvider.
+        when("/",{
+          template: "<blog-list></blog-list>"
+        }).
+        when("/blog/1",{
+            template: "<h1>Hi</h1>"
+        }).
+        when("/blog/2",{
+            template: "<blog-list></blog-list>"
+        }).
+        otherwise({
+          template: "Not Found"
+        })
+    });
--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -1,4 +1,11 @@
 // Module is used for declaring purposes
 'use strict';
 // angular.module('<name of the app>',)
- angular.module('try', ['blogList']);
+ angular.module('try', [
+
+  // external
+  'ngResource',
+  'ngRoute',
+  //  internal
+  'blogList'
+ ]);
